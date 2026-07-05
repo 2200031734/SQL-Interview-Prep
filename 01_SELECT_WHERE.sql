e5352c5a9f16d62d80fd344fec65b7a578131be2
K P S L N MOUKTIKA <162572760+2200031734@users.noreply.github.com>
2026-07-05 17:04:05 +0530

    Update 01_SELECT_WHERE.sql

--- 01_SELECT_WHERE.sql
+++ 01_SELECT_WHERE.sql
@@ -1,1 +1,20 @@
+-- ======================================
+-- BASIC SELECT QUERIES
+-- ======================================
 
+-- Display all employee details
+
+SELECT *
+FROM employees;
+
+-- Display employee names and salaries
+
+SELECT name,
+       salary
+FROM employees;
+
+-- Display employees from Hyderabad
+
+SELECT *
+FROM employees
+WHERE city = 'Hyderabad';
