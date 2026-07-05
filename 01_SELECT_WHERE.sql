4d7d50944d2d561827059a53a3a86f37b4316033
K P S L N MOUKTIKA <162572760+2200031734@users.noreply.github.com>
2026-07-05 17:18:48 +0530

    Update 01_SELECT_WHERE.sql

--- 01_SELECT_WHERE.sql
+++ 01_SELECT_WHERE.sql
@@ -13,8 +13,64 @@ SELECT name,
        salary
 FROM employees;
 
+-- Display employee names and cities
+
+SELECT name,
+       city
+FROM employees;
+
+-- Display employees from Chennai
+
+SELECT *
+FROM employees
+WHERE city = 'Chennai';
+
 -- Display employees from Hyderabad
 
 SELECT *
 FROM employees
 WHERE city = 'Hyderabad';
+
+-- Display employees earning more than 45000
+
+SELECT *
+FROM employees
+WHERE salary > 45000;
+
+-- Display employees earning greater than or equal to 60000
+
+SELECT *
+FROM employees
+WHERE salary >= 60000;
+
+-- Display employees earning less than or equal to 50000
+
+SELECT *
+FROM employees
+WHERE salary <= 50000;
+
+-- Display employees from Hyderabad earning more than 40000
+
+SELECT *
+FROM employees
+WHERE city = 'Hyderabad'
+  AND salary > 40000;
+
+-- Display employees from Chennai or Mumbai
+
+SELECT *
+FROM employees
+WHERE city = 'Chennai'
+   OR city = 'Mumbai';
+
+-- Display names of employees from Hyderabad
+
+SELECT name
+FROM employees
+WHERE city = 'Hyderabad';
+
+-- Display salary of employee named Priya
+
+SELECT salary
+FROM employees
+WHERE name = 'Priya';
