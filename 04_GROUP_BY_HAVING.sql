1b88a6c93d655ebe05c63c7779a07f923b3601cf
K P S L N MOUKTIKA <162572760+2200031734@users.noreply.github.com>
2026-07-05 18:30:38 +0530

    Update 04_GROUP_BY_HAVING.sql

--- 04_GROUP_BY_HAVING.sql
+++ 04_GROUP_BY_HAVING.sql
@@ -1,1 +1,48 @@
+-- ==========================================
+-- SQL Interview Preparation
+-- File: 04_GROUP_BY_HAVING.sql
+-- Author: Mouktika K
+-- ==========================================
 
+-- Count employees in each department
+
+SELECT department,
+       COUNT(*)
+FROM employees
+GROUP BY department;
+
+-- Highest salary in each department
+
+SELECT department,
+       MAX(salary)
+FROM employees
+GROUP BY department;
+
+-- Lowest salary in each department
+
+SELECT department,
+       MIN(salary)
+FROM employees
+GROUP BY department;
+
+-- Total salary in each department
+
+SELECT department,
+       SUM(salary)
+FROM employees
+GROUP BY department;
+
+-- Average salary in each department
+
+SELECT department,
+       AVG(salary)
+FROM employees
+GROUP BY department;
+
+-- Departments having average salary greater than 55000
+
+SELECT department,
+       AVG(salary)
+FROM employees
+GROUP BY department
+HAVING AVG(salary) > 55000;
