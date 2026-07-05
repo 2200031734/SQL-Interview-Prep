c4b4ebfb23b54a64c5b144a459c0538950c4c398
K P S L N MOUKTIKA <162572760+2200031734@users.noreply.github.com>
2026-07-05 18:30:03 +0530

    Update 03_AGGREGATE_FUNCTIONS.sql

--- 03_AGGREGATE_FUNCTIONS.sql
+++ 03_AGGREGATE_FUNCTIONS.sql
@@ -1,1 +1,36 @@
+-- ==========================================
+-- SQL Interview Preparation
+-- File: 03_AGGREGATE_FUNCTIONS.sql
+-- Author: Mouktika K
+-- ==========================================
 
+-- Find the highest salary
+
+SELECT MAX(salary)
+FROM employees;
+
+-- Find the lowest salary
+
+SELECT MIN(salary)
+FROM employees;
+
+-- Find the average salary
+
+SELECT AVG(salary)
+FROM employees;
+
+-- Find the total salary paid
+
+SELECT SUM(salary)
+FROM employees;
+
+-- Count total employees
+
+SELECT COUNT(*)
+FROM employees;
+
+-- Count employees in the IT department
+
+SELECT COUNT(*)
+FROM employees
+WHERE department = 'IT';
